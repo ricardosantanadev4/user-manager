f4c2e75e6ba0ded26ccfc1806cdbbe8dd51e0a14
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2024-12-10 16:58:55 -0300

    feat: Configurações da classe UsuarioDTO.

--- usermanager-spring/src/main/java/com/ricardosantana/spring/usermanager/dtos/UsuarioDTO.java
+++ usermanager-spring/src/main/java/com/ricardosantana/spring/usermanager/dtos/UsuarioDTO.java
@@ -1,5 +1,15 @@
 package com.ricardosantana.spring.usermanager.dtos;
 
-public class UsuarioDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UsuarioDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
 }
