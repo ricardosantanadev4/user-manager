14f553da1a02b216e1d91a28b8ca97b49f5ec7fc
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2024-12-12 09:53:55 -0300

    refactor: Pasta do projeto renomeada.

--- usermanager-spring/src/main/java/com/ricardosantana/spring/usermanager/dtos/UsuarioDTO.java
+++
@@ -1,5 +0,0 @@
-package com.ricardosantana.spring.usermanager.dtos;
-
-public record UsuarioDTO(Long id, String nome, String email, String telefone) {
-
-}
--- usermanager-spring/src/main/java/com/ricardosantana/spring/usermanager/dtos/UsuarioPageDTO.java
+++
@@ -1,7 +0,0 @@
-package com.ricardosantana.spring.usermanager.dtos;
-
-import java.util.List;
-
-public record UsuarioPageDTO(List<UsuarioDTO> usuarios, long totalElements, int totalPages) {
-
-}
