12b340c343c62f014fd37abc9c1493da1a052ef9
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2024-12-18 15:17:18 -0300

    feat: Configurações de rota para o component detalhe-usuario.

--- user-manager-angular/src/app/dashboard/dashboard-routing.module.ts
+++ user-manager-angular/src/app/dashboard/dashboard-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { DetalheUsuarioComponent } from './components/usuario/detalhe-usuario/detalhe-usuario.component';
 
 const routes: Routes = [
   {
     path: '', component: SidenavComponent, children: [
       { path: 'usuarios', component: UsuarioComponent },
+      { path: 'usuarios/detalhe', component: DetalheUsuarioComponent }
     ]
   }
 ];
--- user-manager-angular/src/app/shared/services/usuario.service.ts
+++ user-manager-angular/src/app/shared/services/usuario.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { IUsuarioPage } from '../models/usuario-page.interface';
 import { DaoService } from './dao.service';
 import { AppSettings } from '../../app.settings';
+import { IUsuario } from '../models/usuario.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,12 @@ export class UsuarioService {
   ) { }
 
   public listarTodosOsUsuarios(page: number, size: number): Observable<HttpResponse<IUsuarioPage>> {
-    return this.daoService.get<IUsuarioPage>(`${AppSettings.USUARIOS}?page=${page}&size=${size}`, DaoService.MEDIA_TYPE_APP_JSON);
+    return this.daoService.get<IUsuarioPage>(`${AppSettings.USUARIOS}?page=${page}&size=${size}`,
+      DaoService.MEDIA_TYPE_APP_JSON);
+  }
+
+  public buscarUsuarioPorId(usuarioId: number) {
+    return this.daoService.get<IUsuario>(`${AppSettings.USUARIOS}/${usuarioId}`,
+      DaoService.MEDIA_TYPE_APP_JSON);
   }
 }
