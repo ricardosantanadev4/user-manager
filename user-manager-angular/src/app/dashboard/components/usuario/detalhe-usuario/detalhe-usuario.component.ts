b03f78826d84d56fc5685c1bd9140073669def55
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2024-12-18 11:20:15 -0300

    feat: Criação do component detalhe-usuario.

---
+++ user-manager-angular/src/app/dashboard/components/usuario/detalhe-usuario/detalhe-usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalhe-usuario.component.html',
+  styleUrl: './detalhe-usuario.component.scss'
+})
+export class DetalheUsuarioComponent {
+
+}
