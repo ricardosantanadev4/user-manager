e5a22e2c5500437e386709d6e778fb1cc08ea5bf
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2024-12-20 14:54:00 -0300

    feat: Criação do formulário para cadastro de usuários.

--- user-manager-angular/src/app/dashboard/components/usuario/detalhe-usuario/detalhe-usuario.component.ts
+++ user-manager-angular/src/app/dashboard/components/usuario/detalhe-usuario/detalhe-usuario.component.ts
@@ -1,12 +1,39 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-detalhe-usuario',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './detalhe-usuario.component.html',
   styleUrl: './detalhe-usuario.component.scss'
 })
 export class DetalheUsuarioComponent {
+  form!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = this.formBuilder.group({
+      nome: ['', Validators.required],
+      email: ['', Validators.required],
+      telefone: ['', Validators.required]
+    })
+  }
+
+  novoRegistro() {
+    alert('Novo registro iniciado!');
+  }
+
+  salvar() {
+    if (this.form.valid) {
+      console.log('Dados salvos:', this.form.value);
+      alert('Registro salvo com sucesso!');
+    } else {
+      alert('Por favor, preencha todos os campos antes de salvar.');
+    }
+  }
 }
